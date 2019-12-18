780b2d31f4353ad867dfa3c49baedd87dbe95aa5
liys <18702198569@163.com>
2019-12-18 21:14:38 +0800

    黑马头条89项目初始化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui' // 引入包
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+
+import './styles/index.less' // 引入初始化
 Vue.config.productionTip = false
+Vue.use(ElementUI) // 全局注册
 new Vue({
   router,
   render: h => h(App)
