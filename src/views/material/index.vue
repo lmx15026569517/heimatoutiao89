29192e55b2c3bdd61cd46fab152dd5992df60e27
liys <18702198569@163.com>
2019-12-24 21:44:34 +0800

    素材列表-页面结构

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -4,11 +4,26 @@
     <bread-crumb slot="header">
       <template slot="title">素材管理</template>
     </bread-crumb>
+    <!-- 素材 -->
+    <el-tabs v-model="activeName">
+        <el-tab-pane label="全部素材" name="all">
+        <!-- 全部素材 -->
+        </el-tab-pane>
+        <el-tab-pane label="收藏素材" name="collect">
+        <!-- 收藏素材 -->
+        </el-tab-pane>
+
+    </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      activeName: 'collect'
+    }
+  }
 
 }
 </script>
