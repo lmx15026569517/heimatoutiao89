a14cc76ec349bd250d66e2cbb217b0c3ec54fa73
liys <18702198569@163.com>
2019-12-26 18:18:26 +0800

    黑马头条pc-素材列表 素材上传

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,9 +1,14 @@
 <template>
   <!-- 基本页面结构 -->
-  <el-card>
+  <el-card v-loading=" loading">
     <bread-crumb slot="header">
       <template slot="title">素材管理</template>
     </bread-crumb>
+    <el-row typu='flex' justify="end">
+        <el-upload :http-reques="uploadImg">
+          <el-button size="small" type="primary">点击上传<el-button>
+        </el-upload>
+    </el-row>
     <!-- 素材 -->
     <el-tabs v-model="activeName" @tab-click="changeTab">
       <el-tab-pane label="全部素材" name="all">
@@ -53,6 +58,7 @@
 export default {
   data () {
     return {
+      loading: false, //  定义一个变量
       activeName: 'all', //  默认全部选中
       list: [], // 接收全部数据
       page: {
@@ -63,7 +69,23 @@ export default {
     }
   },
   methods: {
-    //  切换事件
+    // 上传图片
+    uploadImg (params) {
+      this.loading = true // 打开进度条
+      let form = new FormData()
+      form.append('image', params.file) //  添加文件到formData
+      this.$$axios({
+        method: 'post',
+        url: '/user/images',
+        data: form // formData 数据
+      }).then(result => {
+        //  说明已经上传成功了一张图片
+        this.loading = false //  关闭进度条
+        this.getAllMaterial()
+      })
+    },
+
+    //  切换分页
     changrPage (newPage) {
       this.page.currentPage = newPage //  得到一个最新页码
       this.getAllMaterial()
