caf178335c52bea181cfe94e99b327cd5e5871a5
liys <18702198569@163.com>
2019-12-27 20:57:50 +0800

    页面结构删除

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -56,7 +56,7 @@
     </el-row>
     <!-- 主体 -->
     <el-row class='total'>
-        <span>共找到1000条符合条件的内容</span>
+        <span>共找到{{ page.total }}条符合条件的内容</span>
     </el-row>
         <!-- 循环的模板 -->
     <el-row v-for="item in list" :key="item.id.toString()" class='article-item' type='flex' justify="space-between">
@@ -76,11 +76,22 @@
         </el-col>
         <!-- 右侧 -->
         <el-col :span="6">
-             <el-row class='right' type='flex' justify="end">
-               <span><i class="el-icon-edit"></i>修改</span>
-               <span><i class="el-icon-delete"></i>删除</span>
-             </el-row>
+              <el-row class='right' type='flex' justify="end">
+                <span><i class="el-icon-edit"></i>修改</span>
+                <span @click="delArticle(item.id)"><i class="el-icon-delete"></i>删除</span>
+              </el-row>
         </el-col>
+    </el-row>
+    <!-- //分页组件 -->
+    <el-row type='flex' justify="center" align="middle" style='height:60px'>
+        <el-pagination
+           background
+           layout="prev, pager, next"
+           :total="page.total"
+           :current-page="page.currentPage"
+           :page-size="page.pageSize">
+           @current-change=""
+        </el-pagination>
     </el-row>
   </el-card>
 </template>
@@ -96,7 +107,12 @@ export default {
       },
       channels: [], //  定义一个channels接收频道
       list: [], //  接收列表数据
-      defaultImg: require('../../assets/img/lmx1.jpg')
+      defaultImg: require('../../assets/img/lmx1.jpg'),
+      page: {
+        currentPage: 1, //  当前页码
+        pageSize: 10, //  文章列表后台限制最低要求10条
+        total: 0
+      }
     }
   },
   filters: {
@@ -135,18 +151,47 @@ export default {
     }
   },
   methods: {
+    //  删除文章
+    delArticle (id) {
+      //  所有已发布的文章是不可以闪出去的  只有草稿才可以删除
+      this.$confirm('您是否需要删除这个文章').then(() => {
+        //  直接删除
+        this.$axios({
+          methods: 'delete',
+          url: `/articles/${id.toString()}`
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除文章成功!'
+          })
+          // this.page.currentPage = 1  //  假如删除后 回去第一页就加次方法
+          this.getConditionArticle()
+        })
+      })
+    },
+    //  改变页码事件
+    changePage (newPage) {
+      //  赋值当前页码
+      this.page.currentPage = newPage //  赋值当前页页
+      this.getConditionArticle()
+    },
     //  改变条件
     changeCondition () {
       //  组装条件
+      this.page.currentPage = 1 //  强制将当前页码回到第一页
       //  最新状态
+      this.getConditionArticle()
+    },
+    getConditionArticle () {
       let params = {
+        page: this.page.currentPage, //  分页信息
+        per_page: this.page.pageSize, //  分页信息
         status: this.formData.status === 5 ? null : this.formData.status, // 不传为全部  5代表全部
         channel_id: this.formData.channel_id, //  频道
         begin_puddate: this.formData.dateRange.length ? this.formData.dateRange[0] : null, //  起始时间
         end_pubdate: this.formData.dateRange.length > 1 ? this.formData.dateRange[1] : null //  结束时间
-
       }
-      this.getArticles(params) //  调用获取文章数据
+      this.getArticles(params)
     },
     //  获取频道
     getChannels (params) {
@@ -163,12 +208,13 @@ export default {
         url: '/articles' // 请求地址
       }).then(result => {
         this.list = result.data.results //  接收频道列表数据
+        this.page.total = result.data.total_count //  获取文章总数
       })
     }
   },
   created () {
     this.getChannels() // 调用频道 数据
-    this.getArticles() //  调用获取文章列表
+    this.getArticles({ page: 1, per_page: 10 }) //  调用获取文章列表
   }
 }
 </script>
