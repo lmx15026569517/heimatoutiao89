b27609e2c0792fd87e4334b0faf35c3190760116
liys <18702198569@163.com>
2019-12-27 21:35:40 +0800

    发表文章-页面结构-简单实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,6 +3,22 @@
     <bread-crumd slot="head">
         <template slot="title">发表文章</template>
     </bread-crumd>
+    <!-- 表单 label-width-->
+    <el-form style="margin-left:50px">
+        <el-form-item label="标题">
+          <el-input style="width:60%"></el-input>
+          </el-form-item>
+        <el-form-item label="内容">
+          <el-input type='textarea' :rows="4"></el-input>
+        </el-form-item>
+        <el-form-item label="封面"></el-form-item>
+        <el-form-item label="频道"></el-form-item>
+        <el-form-item>
+           <el-button type='primary'>发布</el-button>
+           <el-button>存入草稿</el-button>
+        </el-form-item>
+
+    </el-form>
   </el-card>
 </template>
 
