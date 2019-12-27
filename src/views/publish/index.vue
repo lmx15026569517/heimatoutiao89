b687cf15b9bd71bb6921a655b48c60a362da903b
liys <18702198569@163.com>
2019-12-27 23:58:41 +0800

    发表文章-校验逻辑实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -4,30 +4,32 @@
         <template slot="title">发表文章</template>
     </bread-crumd>
     <!-- 表单 label-width-->
-    <el-form style="margin-left:50px">
-        <el-form-item label="标题">
-          <el-input style="width:60%"></el-input>
+    <el-form :model="formData" :rules="publishRules" style="margin-left:50px">
+        <el-form-item ref="publishForm" prop="title" label="标题">
+          <el-input v-model="formData.title" style="width:60%"></el-input>
           </el-form-item>
-        <el-form-item label="内容">
-          <el-input type='textarea' :rows="4"></el-input>
+        <el-form-item prop="content" label="内容">
+          <el-input v-model="formData.content" type='textarea' :rows="4"></el-input>
         </el-form-item>
-        <el-form-item label="封面"></el-form-item>
-          <el-radio-group>
-            <el-radio>单图</el-radio>
-            <el-radio>三图</el-radio>
-            <el-radio>无图</el-radio>
-            <el-radio>自动</el-radio>
+        <!-- 绑定字段  字段定义的是prop -->
+        <el-form-item prop="type" label="封面"></el-form-item>
+        <!-- 单选组 v-model="封面类型"  -->
+          <el-radio-group v-model="formData.cover.type">
+            <el-radio :lagel="1">单图</el-radio>
+            <el-radio :lagel="3">三图</el-radio>
+            <el-radio :lagel="0">无图</el-radio>
+            <el-radio :lagel="-1">自动</el-radio>
           </el-radio-group>
-        <el-form-item label="频道">
-          <el-select>
+        <el-form-item prop="channel_id" label="频道">
+          <el-select v-model="formData.channel_id">
             <el-option v-for="item in channels" :key="item.id" :value="item.value" :label="item.name">
 
             </el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
-           <el-button type='primary'>发布</el-button>
-           <el-button>存入草稿</el-button>
+           <el-button @click="publishArticle" type='primary'>发布</el-button>
+           <el-button @click="publishArticle">存入草稿</el-button>
         </el-form-item>
 
     </el-form>
@@ -38,7 +40,25 @@
 export default {
   data () {
     return {
-      channels: [] //  定义一个channels接收频道
+      channels: [], //  定义一个channels接收频道
+      formData: {
+        title: '', //  文章 标题
+        content: '', // 文章内容
+        cover: {
+          type: 0, //  封面类型 -1:自动  0无图. 1-1张  3-3张
+          images: []//  存储的图片地址
+        },
+        channel_id: null // 频道id
+      },
+      publishRules: {
+        // 校验规则对象 min max  (校验方法)
+        title: [{ required: true, message: '标题不能为空' }, {
+          min: 5, max: 30, message: '标题长度需要在5-30字符之间'
+        }],
+        content: [{ required: true, message: '文章内容不能为空' }],
+        channel_id: [{ required: true, message: '频道分类不能为空' }]
+
+      }
     }
   },
   methods: {
@@ -49,10 +69,17 @@ export default {
       }).then(result => {
         this.channels = result.data.channels //  获取频道数据
       })
+    },
+    publishArticle () {
+      this.$refs.publishForm.validate(function (isOk) {
+        if (isOk) {
+          // 可以进行发布接口
+        }
+      })
+    },
+    created () {
+      this.getChannels() // 获取频道数据
     }
-  },
-  created () {
-    this.getChannels() // 获取频道数据
   }
 }
 </script>
