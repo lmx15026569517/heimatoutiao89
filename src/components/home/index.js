adada4119ca5ef6bb7d990f0808e5eca77d14b61
liys <18702198569@163.com>
2019-12-29 00:35:42 +0800

    封面图片选择组件封装使用上

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,6 +3,8 @@ import LayoutHeader from '../layout-header'
 import BreadCrumb from '../common/bread-crumb.vue'
 import { quillEditor } from 'vue-quill-editor' // 引入了quillEditor组件
 import CoverImage from '../publish/cover-image' // 路径和老师的不一样 因为文件夹
+import SelectImage from '../publish/select-image'
+
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
@@ -14,5 +16,6 @@ export default {
     Vue.component('bread-crumb', BreadCrumb) // 全局注册一个面包屑组件
     Vue.component('quill-editor', quillEditor)//  全局注册一个富文本
     Vue.component('cover-image', CoverImage) // 注册一个封面组件
+    Vue.component('select-image', SelectImage) //  注册一个选择素材组件
   }
 }
